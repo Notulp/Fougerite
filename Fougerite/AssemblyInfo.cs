4160ee45083a0fdb6180440ffc56e3c1b1064267
DreTaX <dretax14@gmail.com>
2022-05-02 12:33:49 +0200

    Some refactoring.

--- Fougerite/AssemblyInfo.cs
+++ Fougerite/AssemblyInfo.cs
@@ -3,7 +3,7 @@
 [assembly: System.Reflection.AssemblyTrademark("")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.Runtime.InteropServices.Guid("356720a4-fa40-4b47-91d2-b2c21a6b682e")]
-[assembly: System.Reflection.AssemblyCopyright("Copyright © 2015-2016 Pluton Team")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright © 2015-2022 Pluton Team")]
 [assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
 [assembly: System.Reflection.AssemblyFileVersion("1.3.8")]
 [assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
--- Fougerite/Fougerite/PluginLoaders/Singleton.cs
+++ Fougerite/Fougerite/PluginLoaders/Singleton.cs
@@ -4,25 +4,24 @@ namespace Fougerite.PluginLoaders
 {
     public abstract class Singleton<T> : CountedInstance where T : ISingleton
     {
-        private static T Instance;
+        private static readonly T Instance;
 
         public static T GetInstance()
         {
-            return Singleton<T>.Instance;
+            return Instance;
         }
 
         static Singleton()
         {
-            Singleton<T>.Instance = Activator.CreateInstance<T>();
-            if (Singleton<T>.Instance.CheckDependencies())
+            Instance = Activator.CreateInstance<T>();
+            if (Instance.CheckDependencies())
             {
-                Singleton<T>.Instance.Initialize();
+                Instance.Initialize();
             }
             else
             {
                 Logger.LogWarning(Instance.GetType() + " is disabled in the Fougerite.cfg, and will not load any plugins.");
             }
-
         }
     }
 }
