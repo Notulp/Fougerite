3da11dfdca04f735e1888021d6538287b9750d1b
DreTaX <dretax14@gmail.com>
2026-05-06 10:48:45 +0200

    docs

--- Fougerite/Fougerite/ScriptWebSocket.cs
+++ Fougerite/Fougerite/ScriptWebSocket.cs
@@ -10,6 +10,7 @@ namespace Fougerite
     /// Represents a WebSocket client implementation using native WinHTTP.
     /// Designed to provide secure, background-threaded WebSocket connections for plugins.
     /// Warning: This class is not thread safe. Only Connect and Close from the main thread ideally from a single plugin.
+    /// Send operation can be called from any thread.
     /// </summary>
     public class ScriptWebSocket : IDisposable
     {
