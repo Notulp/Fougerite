d07e3802e12fd27b9420323825ea5a242391fa34
DreTaX <dretax14@gmail.com>
2026-05-24 14:40:20 +0200

    Refine ConcurrentDictionary

--- Fougerite/Fougerite/Concurrent/ConcurrentDictionary.cs
+++ Fougerite/Fougerite/Concurrent/ConcurrentDictionary.cs
@@ -2,35 +2,65 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace Fougerite.Concurrent
 {
     /// <summary>
-    /// A .NET 3.5 implementation of ConcurrentDictionary, using ReaderWriterLock.
-    /// You should read the microsoft docs on how a ConcurrentDictionary works.
+    /// A thread-safe implementation of a Dictionary for .NET 3.5 using ReaderWriterLock.
+    /// Allows multiple concurrent readers or a single exclusive writer.
     /// </summary>
-    /// <typeparam name="TKey"></typeparam>
-    /// <typeparam name="TValue"></typeparam>
+    /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
     public class ConcurrentDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
     {
         private readonly ReaderWriterLock _padlock = new ReaderWriterLock();
-        private readonly Dictionary<TKey, TValue> _dictionary = new Dictionary<TKey, TValue>();
+        private readonly Dictionary<TKey, TValue> _dictionary;
         
         /// <summary>
         /// Initializes a new, empty instance of the ConcurrentDictionary class.
         /// </summary>
         public ConcurrentDictionary()
         {
-            
+            _dictionary = new Dictionary<TKey, TValue>();
         }
-        
+
         /// <summary>
-        /// Initializes a new instance of the ConcurrentDictionary class containing elements from the specified dictionary.
+        /// Initializes a new, empty instance of the ConcurrentDictionary class that has the specified initial capacity.
+        /// </summary>
+        /// <param name="capacity">The initial number of elements that the dictionary can contain.</param>
+        public ConcurrentDictionary(int capacity)
+        {
+            _dictionary = new Dictionary<TKey, TValue>(capacity);
+        }
+
+        /// <summary>
+        /// Initializes a new, empty instance of the ConcurrentDictionary class that uses the specified IEqualityComparer.
+        /// </summary>
+        /// <param name="comparer">The IEqualityComparer implementation to use when comparing keys.</param>
+        public ConcurrentDictionary(IEqualityComparer<TKey> comparer)
+        {
+            _dictionary = new Dictionary<TKey, TValue>(comparer);
+        }
+
+        /// <summary>
+        /// Initializes a new, empty instance of the ConcurrentDictionary class with specified capacity and IEqualityComparer.
+        /// </summary>
+        /// <param name="capacity">The initial number of elements that the dictionary can contain.</param>
+        /// <param name="comparer">The IEqualityComparer implementation to use when comparing keys.</param>
+        public ConcurrentDictionary(int capacity, IEqualityComparer<TKey> comparer)
+        {
+            _dictionary = new Dictionary<TKey, TValue>(capacity, comparer);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the ConcurrentDictionary class containing elements copied from the specified dictionary.
         /// </summary>
         /// <param name="originalDict">The dictionary whose elements are copied to the new ConcurrentDictionary.</param>
         public ConcurrentDictionary(Dictionary<TKey, TValue> originalDict)
         {
-            _dictionary = originalDict;
+            if (originalDict == null) throw new ArgumentNullException(nameof(originalDict));
+            _dictionary = new Dictionary<TKey, TValue>(originalDict);
         }
 
         /// <summary>
@@ -42,17 +72,26 @@ namespace Fougerite.Concurrent
         {
             get
             {
-                lock (_padlock)
+                _padlock.AcquireReaderLock(Timeout.Infinite);
+                try
                 {
                     return _dictionary[key];
                 }
+                finally
+                {
+                    _padlock.ReleaseReaderLock();
+                }
             }
-
             set
             {
-                lock (_padlock)
+                _padlock.AcquireWriterLock(Timeout.Infinite);
+                try
                 {
                     _dictionary[key] = value;
+                }
+                finally
+                {
+                    _padlock.ReleaseWriterLock();
                 }
             }
         }
@@ -64,10 +103,15 @@ namespace Fougerite.Concurrent
         /// <returns>An enumerator for the dictionary contents.</returns>
         public IEnumerator GetEnumerator()
         {
-            lock (_padlock)
+            _padlock.AcquireReaderLock(Timeout.Infinite);
+            try
             {
                 // ToList() creates a snapshot to prevent "Collection Modified" exceptions during iteration
                 return ((IEnumerable)_dictionary.ToList()).GetEnumerator();
+            }
+            finally
+            {
+                _padlock.ReleaseReaderLock();
             }
         }
         
@@ -79,9 +123,14 @@ namespace Fougerite.Concurrent
         /// <returns>The value if found, otherwise null (or default).</returns>
         public TValue GetItem(TKey key)
         {
-            lock (_padlock)
+            _padlock.AcquireReaderLock(Timeout.Infinite);
+            try
             {
                 return _dictionary.ContainsKey(key) ? _dictionary[key] : default(TValue);
+            }
+            finally
+            {
+                _padlock.ReleaseReaderLock();
             }
         }
 
@@ -91,9 +140,14 @@ namespace Fougerite.Concurrent
         /// <returns>A shallow copy of the internal dictionary.</returns>
         public Dictionary<TKey, TValue> GetShallowCopy()
         {
-            lock (_padlock)
+            _padlock.AcquireReaderLock(Timeout.Infinite);
+            try
             {
                 return new Dictionary<TKey, TValue>(_dictionary);
+            }
+            finally
+            {
+                _padlock.ReleaseReaderLock();
             }
         }
 
@@ -105,8 +159,15 @@ namespace Fougerite.Concurrent
         /// <returns>true if the key was found, otherwise, false.</returns>
         public bool TryGetValue(TKey key, out TValue value)
         {
-            lock (_padlock)
+            _padlock.AcquireReaderLock(Timeout.Infinite);
+            try
+            {
                 return _dictionary.TryGetValue(key, out value);
+            }
+            finally
+            {
+                _padlock.ReleaseReaderLock();
+            }
         }
 
         /// <summary>
@@ -117,15 +178,19 @@ namespace Fougerite.Concurrent
         /// <returns>true if the key/value pair was added, false if the key already exists.</returns>
         public bool TryAdd(TKey key, TValue value)
         {
-            lock (_padlock)
+            _padlock.AcquireWriterLock(Timeout.Infinite);
+            try
             {
                 if (!_dictionary.ContainsKey(key))
                 {
                     _dictionary.Add(key, value);
                     return true;
                 }
-
                 return false;
+            }
+            finally
+            {
+                _padlock.ReleaseWriterLock();
             }
         }
 
@@ -136,13 +201,19 @@ namespace Fougerite.Concurrent
         /// <returns>true if the object was removed successfully, otherwise, false.</returns>
         public bool TryRemove(TKey key)
         {
-            lock (_padlock)
+            _padlock.AcquireWriterLock(Timeout.Infinite);
+            try
             {
                 if (_dictionary.ContainsKey(key))
+                {
                     return _dictionary.Remove(key);
+                }
+                return false;
             }
-
-            return false;
+            finally
+            {
+                _padlock.ReleaseWriterLock();
+            }
         }
 
         /// <summary>
@@ -151,10 +222,17 @@ namespace Fougerite.Concurrent
         /// <param name="key">The key of the element to remove.</param>
         public void Remove(TKey key)
         {
-            lock (_padlock)
+            _padlock.AcquireWriterLock(Timeout.Infinite);
+            try
             {
                 if (_dictionary.ContainsKey(key))
+                {
                     _dictionary.Remove(key);
+                }
+            }
+            finally
+            {
+                _padlock.ReleaseWriterLock();
             }
         }
 
@@ -163,9 +241,14 @@ namespace Fougerite.Concurrent
         /// </summary>
         public void Add(TKey key, TValue val)
         {
-            lock (_padlock)
+            _padlock.AcquireWriterLock(Timeout.Infinite);
+            try
             {
                 _dictionary.Add(key, val);
+            }
+            finally
+            {
+                _padlock.ReleaseWriterLock();
             }
         }
 
@@ -176,9 +259,14 @@ namespace Fougerite.Concurrent
         /// <returns>true if the key is found, otherwise, false.</returns>
         public bool ContainsKey(TKey id)
         {
-            lock (_padlock)
+            _padlock.AcquireReaderLock(Timeout.Infinite);
+            try
             {
                 return _dictionary.ContainsKey(id);
+            }
+            finally
+            {
+                _padlock.ReleaseReaderLock();
             }
         }
         
@@ -187,9 +275,14 @@ namespace Fougerite.Concurrent
         /// </summary>
         public void Clear()
         {
-            lock (_padlock)
+            _padlock.AcquireWriterLock(Timeout.Infinite);
+            try
             {
                 _dictionary.Clear();
+            }
+            finally
+            {
+                _padlock.ReleaseWriterLock();
             }
         }
 
@@ -198,8 +291,15 @@ namespace Fougerite.Concurrent
         /// </summary>
         public List<KeyValuePair<TKey, TValue>> OrderBy(Func<KeyValuePair<TKey, TValue>, TKey> func)
         {
-            lock (_padlock)
+            _padlock.AcquireReaderLock(Timeout.Infinite);
+            try
+            {
                 return _dictionary.OrderBy(func).ToList();
+            }
+            finally
+            {
+                _padlock.ReleaseReaderLock();
+            }
         }
         
         /// <summary>
@@ -210,9 +310,14 @@ namespace Fougerite.Concurrent
         {
             get 
             {
-                lock (_padlock) 
+                _padlock.AcquireReaderLock(Timeout.Infinite);
+                try
                 {
                     return _dictionary.Count;
+                }
+                finally
+                {
+                    _padlock.ReleaseReaderLock();
                 }
             }
         }
@@ -224,9 +329,14 @@ namespace Fougerite.Concurrent
         {
             get
             {
-                lock (_padlock)
+                _padlock.AcquireReaderLock(Timeout.Infinite);
+                try
                 {
                     return new List<TValue>(_dictionary.Values);
+                }
+                finally
+                {
+                    _padlock.ReleaseReaderLock();
                 }
             }
         }
@@ -238,37 +348,54 @@ namespace Fougerite.Concurrent
         {
             get
             {
-                lock (_padlock)
+                _padlock.AcquireReaderLock(Timeout.Infinite);
+                try
                 {
                     return new List<TKey>(_dictionary.Keys);
+                }
+                finally
+                {
+                    _padlock.ReleaseReaderLock();
                 }
             }
         }
 
         /// <summary>
         /// Gets a collection containing the values in the dictionary.
+        /// NOTE: Iterating this outside a lock is thread-unsafe. Use ValuesCopy for safe iteration.
         /// </summary>
         public Dictionary<TKey, TValue>.ValueCollection Values
         {
             get
             {
-                lock (_padlock)
+                _padlock.AcquireReaderLock(Timeout.Infinite);
+                try
                 {
                     return _dictionary.Values;
+                }
+                finally
+                {
+                    _padlock.ReleaseReaderLock();
                 }
             }
         }
 
         /// <summary>
         /// Gets a collection containing the keys in the dictionary.
+        /// NOTE: Iterating this outside a lock is thread-unsafe. Use KeysCopy for safe iteration.
         /// </summary>
         public Dictionary<TKey, TValue>.KeyCollection Keys
         {
             get
             {
-                lock (_padlock)
+                _padlock.AcquireReaderLock(Timeout.Infinite);
+                try
                 {
                     return _dictionary.Keys;
+                }
+                finally
+                {
+                    _padlock.ReleaseReaderLock();
                 }
             }
         }
@@ -278,9 +405,15 @@ namespace Fougerite.Concurrent
         /// </summary>
         IEnumerator<KeyValuePair<TKey, TValue>> IEnumerable<KeyValuePair<TKey, TValue>>.GetEnumerator()
         {
-            lock (_padlock)
+            _padlock.AcquireReaderLock(Timeout.Infinite);
+            try
             {
-                return _dictionary.GetEnumerator();
+                // ToList() prevents "Collection was modified" exceptions
+                return _dictionary.ToList().GetEnumerator();
+            }
+            finally
+            {
+                _padlock.ReleaseReaderLock();
             }
         }
         
@@ -289,9 +422,14 @@ namespace Fougerite.Concurrent
         /// </summary>
         IEnumerator IEnumerable.GetEnumerator()
         {
-            lock (_padlock)
+            _padlock.AcquireReaderLock(Timeout.Infinite);
+            try
             {
-                return _dictionary.GetEnumerator();
+                return _dictionary.ToList().GetEnumerator();
+            }
+            finally
+            {
+                _padlock.ReleaseReaderLock();
             }
         }
     }
